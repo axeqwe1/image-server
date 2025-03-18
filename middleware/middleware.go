381a08c041a672a9223bbb3f0a6448e6939396f5
Bunniphat.l <shuffer2543@gmail.com>
2025-03-18 17:21:27 +0700

    fix config and fix middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,24 +1,24 @@
 package middleware
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 // Cors middleware เพื่อจัดการ Cross-Origin Resource Sharing
 func Cors() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// ตั้งค่า header พื้นฐานสำหรับ CORS
-		c.Set("Access-Control-Allow-Origin", "*") // อนุญาตทุก origin (ปรับได้)
+		c.Set("Access-Control-Allow-Origin", "*")
 		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
-		c.Set("Access-Control-Max-Age", "86400") // Cache preflight request 24 ชม.
+		c.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Requested-With")
+		c.Set("Access-Control-Max-Age", "86400")
 
-		// ถ้าเป็น preflight request (OPTIONS) ให้ตอบกลับทันที
 		if c.Method() == fiber.MethodOptions {
+			fmt.Println("Preflight request received")
 			return c.SendStatus(fiber.StatusNoContent)
 		}
 
-		// ดำเนินการต่อไปยัง handler ถัดไป
 		return c.Next()
 	}
 }
